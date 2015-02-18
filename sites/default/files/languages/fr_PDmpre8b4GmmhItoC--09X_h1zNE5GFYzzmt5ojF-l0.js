74bd567149a0ad6ccfd3226bacc9786785820dbd
yvandouenel <yvan@coopernet.fr>
2015-02-18 20:51:26 +0100

    Drupal 7.34 and module l10n_update added.

---
+++ sites/default/files/languages/fr_PDmpre8b4GmmhItoC--09X_h1zNE5GFYzzmt5ojF-l0.js
@@ -0,0 +1,1 @@
+Drupal.locale = { 'pluralFormula': function ($n) { return Number(($n>1)); }, 'strings': {"":{"Configure":"Configurer","Show shortcuts":"Afficher les raccourcis","@title dialog":"dialogue de @title","Hide shortcuts":"Cacher les raccourcis"}} };
