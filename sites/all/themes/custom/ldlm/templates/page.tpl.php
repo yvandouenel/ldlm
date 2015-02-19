47f857b1df68c57148c9e626805c2787e578ddb5
yvandouenel <yvan@coopernet.fr>
2015-02-19 16:15:17 +0100

    page.tpl.php modified.

--- sites/all/themes/custom/ldlm/templates/page.tpl.php
+++ sites/all/themes/custom/ldlm/templates/page.tpl.php
@@ -1,11 +1,11 @@
 <section id="top_bar">
-  <div class="center">
+  <div class="container">
     <?php print render($page['top_bar']); ?>
   </div>
 </section>
 <section id="head_band">
-  <div class="center">
-    <h1 id="paul_bousquet"><a href="."><img src="<?php print base_path() . path_to_theme(); ?>/images/logo-Paul-Bousquet.gif" alt="Paul Bousquet"></a></h1>
+  <div class="container">
+    <h1 id="paul_bousquet"><a href="<?php print $front_page; ?>"><img src="<?php print base_path() . path_to_theme(); ?>/images/logo-Paul-Bousquet.gif" alt="Paul Bousquet"></a></h1>
     <a id="link_ldlm" href="/vie-lyceenne">Lycée de la mer</a><a id="link_pdfm" href="/pole-formation-maritime">Pôle de formation maritime</a>
     <nav id="main_menu">
       <?php print render($page['main_menu']); ?>
@@ -13,7 +13,7 @@
   </div>
 </section>
 <section id="main_content">
-  <div class="center">
+  <div class="container">
     <?php if ($messages): ?>
     <div id="messages"><div class="section clearfix">
       <?php print $messages; ?>
@@ -36,7 +36,13 @@
           <?php print render($action_links); ?>
         </ul>
       <?php endif; ?>
-      
+   
       <?php print render($page['content']); ?>
+      <?php if ($page['home_col1']): ?>
+        <div class='grid-6' class='clearfix'><?php print render($page['home_col1']) ?></div>
+      <?php endif; ?>
+        <div class='grid-3' class='clearfix'>1<?php print render($page['home_col2']) ?></div>
+        <div class='grid-3' class='clearfix'>2<?php print render($page['home_col3']) ?></div>
+
 </div>
 </section>
